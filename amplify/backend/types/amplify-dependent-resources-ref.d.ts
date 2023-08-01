efb44a5beef0ae2a3e17eaa38c5c0585ad050d7c
carlos <carlos_gallegos2095@outlook.com>
2023-08-01 14:01:03 -0600

    update for login and signup with amp
    this has the initial set up for login part

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amptest83189488": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
